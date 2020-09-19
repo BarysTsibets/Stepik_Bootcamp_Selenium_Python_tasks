3e92666e6003200acd22a31064a59c607f74b844
Barys_Tsibets <molekyla_08@mail.ru>
2020-09-18 18:27:25 -0700

    lesson3: example added

--- lesson3_step.py
+++ lesson3_step.py
@@ -1,14 +1,8 @@
-# def test_input_text(expected_result, actual_result):
-#     assert expected_result == actual_result, \
-#         f"expected {expected_result}, got {actual_result}"
-#     if True:
-#         return f"{expected_result} {actual_result}"
-#
-#
-# print(test_input_text(11, 12))
+s = 'My Name is Julia'
 
+if 'Name' in s:
+    print('Substring found')
 
-def test_input_text(expected_result, actual_result):
-    assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
-
-test_input_text(9, 9)
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
