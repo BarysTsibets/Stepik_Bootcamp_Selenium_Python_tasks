adc4ad44f66e3b4da172ae2907e84850f76a0da3
Barys_Tsibets <molekyla_08@mail.ru>
2020-09-22 17:40:19 -0700

    Example with and witOUT fixtures

---
+++ test_osa.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+
