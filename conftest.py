f554c2745e007e86be4729b21d0d28b49236370e
Barys_Tsibets <molekyla_08@mail.ru>
2020-09-29 17:16:52 -0700

    lessonn3_6 (pytest -s -v --browser_name=chrome test_conftest.py.py)

--- conftest.py
+++ conftest.py
@@ -1,14 +1,26 @@
-from selenium import webdriver
 import pytest
+from selenium import webdriver
 
+PATH1 = r'C:\Users\BorisPC\PycharmProjects\untitled\HelloWorld\Automation\driver\chromedriver.exe'
+PATH2 = r'C:\Users\BorisPC\PycharmProjects\untitled\HelloWorld\Automation\driver\geckodriver.exe'
 
-PATH = r'C:\Users\BorisPC\PycharmProjects\untitled\HelloWorld\Automation\driver\chromedriver.exe'
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(executable_path=PATH)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(executable_path=PATH1)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(executable_path=PATH2)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
